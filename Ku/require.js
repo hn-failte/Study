bee4ab8afe4fd16c683abf0e82eec2632d7f347f
hn-failte <failte@163.com>
2020-04-23 21:58:20 +0800

    add error handle

--- Ku/require.js
+++ Ku/require.js
@@ -2,19 +2,27 @@ let modules = {};
 
 const config = configModules => {
   Object.keys(configModules).forEach(item => {
-      modules[item] = {path: configModules[item]}
-  })
+    modules[item] = { path: configModules[item] };
+  });
 };
 
 const require = moduleName => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const script = document.createElement("script");
     script.src = modules[moduleName].path;
     script.onload = () => {
-      resolve(modules[moduleName].callback())
+      resolve(modules[moduleName].callback());
+    };
+    script.onerror = () => {
+      reject(modules[moduleName].path);
     };
     document.documentElement.appendChild(script);
-  });
+  }).then(
+    res => res,
+    rej => {
+      return new Error("can not load " + rej);
+    }
+  );
 };
 
 const define = (moduleName, callback) => {
