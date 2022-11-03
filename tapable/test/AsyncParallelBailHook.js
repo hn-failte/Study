8f50757c0646c903c313219ecfc85f577da69fe0
hn-failte <failte@163.com>
2022-11-03 23:17:45 +0800

    feat: tapable完善1103

--- tapable/test/AsyncParallelBailHook.js
+++ tapable/test/AsyncParallelBailHook.js
@@ -1,39 +1,80 @@
 const { AsyncParallelBailHook } = require("../lib");
 
 // 异步并行保释 Hook
+
+/**
+ * Bail 执行每个事件函数，遇到结果不为undefined则结束执行，直接走call的回调
+ */
 const hooks = new AsyncParallelBailHook(["params"]);
 
 hooks.tapPromise("a", (params, callback) => {
-  // tapPromise 不带有回调，因此无法使用promise进行保释
-  console.log(callback, 'callback');
+  // tapPromise 通过 resolve 进行保释
+  console.log('a tapPromise callback:', !callback ? 'not' : '', 'exist');
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("hooks a", params);
-      resolve(void 0);
+      console.log("hooks 800 a tapPromise", params);
+      resolve("a tapPromise 800");
+      callback && callback('a tapPromise 800')
     }, 800);
   });
 });
 
 hooks.tapAsync("b", (params, callback) => {
+  console.log('b tapAsync callback:', !callback ? 'not' : '', 'exist');
   setTimeout(() => {
-    console.log("hooks b", params);
+    console.log("hooks 300 b tapAsync", params);
     // 进行保释，该操作会使回调在所以 tap 运行完成后执行
-    callback(true);
+    callback && callback("b tapAsync 300");
   }, 300);
 });
 
 hooks.tapAsync("c", (params, callback) => {
+  console.log('c tapAsync callback:', !callback ? 'not' : '', 'exist');
   setTimeout(() => {
-    console.log("hooks c", params);
+    console.log("hooks 500 c tapAsync", params);
+    callback && callback("c tapAsync 500")
   }, 500);
 });
 
 hooks.tap('d', (params, callback) => {
   // tap 同样不带有回调，因此无法使用promise进行保释
-  console.log(callback, 'callback');
-  console.log("hooks d", params);
+  console.log('d tap callback:', !callback ? 'not' : '', 'exist');
+  console.log("hooks d tap", params);
+  callback && callback("d tap sync")
 })
 
-hooks.callAsync("start", () => {
-  console.log("done");
+hooks.tapPromise("e", (params, callback) => {
+  // tapPromise 通过 resolve 进行保释
+  console.log('e tapPromise callback:', !callback ? 'not' : '', 'exist');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("hooks 1000 e tapPromise", params);
+      resolve("e tapPromise 1000");
+      callback && callback('e tapPromise 1000')
+    }, 1000);
+  });
+});
+
+hooks.tapPromise("f", (params, callback) => {
+  // tapPromise 通过 resolve 进行保释
+  console.log('f tapPromise callback:', !callback ? 'not' : '', 'exist');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("hooks 600 f tapPromise", params);
+      resolve("f tapPromise 600");
+      callback && callback('f tapPromise 600')
+    }, 600);
+  });
+});
+
+hooks.tapAsync("j", (params, callback) => {
+  console.log('g tapAsync callback:', !callback ? 'not' : '', 'exist');
+  setTimeout(() => {
+    console.log("hooks 900 g tapAsync", params);
+    callback && callback("g tapAsync 900")
+  }, 900);
+});
+
+hooks.callAsync("start", (...res) => {
+  console.log("done", res);
 });
--- tapable/test/AsyncParallelHook.js
+++ tapable/test/AsyncParallelHook.js
@@ -1,27 +1,51 @@
 const { AsyncParallelHook } = require("../lib");
 
 // 异步并行 Hook
+// 该 Hook 的流程类似与 Promise.all
+// 该 Hook 的所有钩子中，钩子回调的第一个参数均为事务的 key
+// 该 Hook 的 tapAsync 钩子中，钩子回调的第二个参数为事务回调的参数，该函数只会执行一遍，且其在一个函数中执行后，不会管是否有其他钩子未执行完
 const hooks = new AsyncParallelHook(["params"]);
 
-hooks.tapPromise("a", () => {
+hooks.tapPromise("a", (tapKey, ...rest) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("hooks a");
-      resolve();
+      console.log("hooks a", tapKey);
+      resolve('a 1000');
     }, 1000);
   });
 });
 
-hooks.tapPromise("b", () => {
+hooks.tapPromise("b", (tapKey) => {
   // a、b 同步执行，b 会先完成
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("hooks b");
-      resolve();
+      console.log("hooks b", tapKey);
+      resolve('b 500');
     }, 500);
   });
 });
 
-hooks.callAsync("start", () => {
-  console.log("done");
+// 后执行回调的没有触发回调
+hooks.tapAsync("d", (tapKey, callback) => {
+  setTimeout(() => {
+    console.log("hooks d", tapKey);
+    callback('d 2000')
+  }, 2000);
+});
+
+// 先执行回调的没有触发回调
+hooks.tapAsync("e", (tapKey, callback) => {
+  setTimeout(() => {
+    console.log("hooks e", tapKey);
+    callback('e 1500')
+  }, 1500);
+});
+
+hooks.tap('c', (tapKey, callback) => {
+  console.log(callback)
+  console.log('c', tapKey)
+})
+
+hooks.callAsync("start", (...res) => {
+  console.log("done", res);
 });
